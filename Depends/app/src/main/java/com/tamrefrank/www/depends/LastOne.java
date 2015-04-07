f223b9db5a3f3f24f12e8a1bee7f5adbd604ef94
tamzi <frnktamre@gmail.com>
2015-04-07 11:18:06 +0300

    Added a list of Apps
    Moringa School Cohort2 Android Examples added

---
+++ Depends/app/src/main/java/com/tamrefrank/www/depends/LastOne.java
@@ -0,0 +1,12 @@
+package com.tamrefrank.www.depends;
+
+/**
+ * Created by v on 01-Apr-15.
+ */
+public class LastOne {
+
+
+
+
+
+}
---
+++ Depends/app/src/main/java/com/tamrefrank/www/depends/Splash.java
@@ -0,0 +1,7 @@
+package com.tamrefrank.www.depends;
+
+/**
+ * Created by v on 01-Apr-15.
+ */
+public class Splash {
+}
